250c5d861dca3323ee117e5094c1b4c9d84dd657
marco76tv <marco76tv@users.noreply.github.com>
2024-11-27 13:03:12 +0000

    Check & fix styling

--- Actions/Array/SaveArrayAction.php
+++ Actions/Array/SaveArrayAction.php
@@ -23,6 +23,5 @@ class SaveArrayAction
         // $content = str_replace('stdClass::__set_state', '(object)', $content);
         File::makeDirectory(\dirname((string) $filename), 0775, true, true);
         File::put($filename, $content);
-        
     }
 }
--- Actions/Filament/AutoLabelAction.php
+++ Actions/Filament/AutoLabelAction.php
@@ -9,10 +9,10 @@ declare(strict_types=1);
 namespace Modules\Xot\Actions\Filament;
 
 use Illuminate\Support\Arr;
-use Webmozart\Assert\Assert;
 use Modules\Lang\Actions\SaveTransAction;
 use Modules\Xot\Actions\GetTransKeyAction;
 use Spatie\QueueableAction\QueueableAction;
+use Webmozart\Assert\Assert;
 
 class AutoLabelAction
 {
@@ -30,16 +30,15 @@ class AutoLabelAction
         $label_key = $trans_key.'.fields.'.$component->getName().'.label';
         $label = trans($label_key);
         if (is_string($label)) {
-            if($label_key == $label){
-                
+            if ($label_key == $label) {
                 $label_value = $component->getName();
                 $label_key1 = $trans_key.'.fields.'.$component->getName();
                 $label1 = trans($label_key1);
-                if($label_key1 != $label1){
+                if ($label_key1 != $label1) {
                     $label_value = $label1;
                 }
-                
-                app(SaveTransAction::class)->execute($label_key,$label_value);
+
+                app(SaveTransAction::class)->execute($label_key, $label_value);
             }
             $component->label($label);
         }
