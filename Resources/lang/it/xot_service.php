570afd3ac296ffce1668d68ff933319c5d99ebf7
marco xot <marco.sottana@gmail.com>
2024-11-27 09:41:31 +0100

    up

---
+++ Resources/lang/it/xot_service.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'fields' => 
+  array (
+    'matr' => 
+    array (
+      'label' => 'matr',
+    ),
+    'cognome' => 
+    array (
+      'label' => 'cognome',
+    ),
+    'value' => 
+    array (
+      'label' => 'value',
+    ),
+    'remember' => 
+    array (
+      'label' => 'remember',
+    ),
+    'password' => 
+    array (
+      'label' => 'password',
+    ),
+  ),
+);
