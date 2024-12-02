9c78357434c25f6bccc1453979bae8e669a2fe47
Davide Tricella <dadi.tricella@gmail.com>
2024-12-02 15:37:53 +0100

    .

--- Resources/lang/it/xot_service.php
+++ Resources/lang/it/xot_service.php
@@ -1,60 +1,68 @@
-<?php return array (
-  'fields' => 
-  array (
-    'matr' => 
-    array (
-      'label' => 'matr',
+<?php
+return array(
+    'fields' =>
+    array(
+        'matr' =>
+        array(
+            'label' => 'matr',
+        ),
+        'cognome' =>
+        array(
+            'label' => 'cognome',
+        ),
+        'value' =>
+        array(
+            'label' => 'value',
+        ),
+        'remember' =>
+        array(
+            'label' => 'remember',
+        ),
+        'password' =>
+        array(
+            'label' => 'password',
+        ),
+        'email' =>
+        array(
+            'label' => 'email',
+        ),
+        'updated_at' =>
+        array(
+            'label' => 'updated_at',
+        ),
+        'created_at' =>
+        array(
+            'label' => 'created_at',
+        ),
+        'password_expires_at' =>
+        array(
+            'label' => 'password_expires_at',
+        ),
+        'values' =>
+        array(
+            'label' => 'values',
+        ),
+        'assetType' =>
+        array(
+            'name' =>
+            array(
+                'label' => 'assetType.name',
+            ),
+        ),
+        'description' =>
+        array(
+            'label' => 'description',
+        ),
+        'parent_id' =>
+        array(
+            'label' => 'parent_id',
+        ),
+        'creator' =>
+        array(
+            'name' =>
+            array(
+                'label' => 'creator.name',
+            ),
+        ),
     ),
-    'cognome' => 
-    array (
-      'label' => 'cognome',
-    ),
-    'value' => 
-    array (
-      'label' => 'value',
-    ),
-    'remember' => 
-    array (
-      'label' => 'remember',
-    ),
-    'password' => 
-    array (
-      'label' => 'password',
-    ),
-    'email' => 
-    array (
-      'label' => 'email',
-    ),
-    'updated_at' => 
-    array (
-      'label' => 'updated_at',
-    ),
-    'created_at' => 
-    array (
-      'label' => 'created_at',
-    ),
-    'password_expires_at' => 
-    array (
-      'label' => 'password_expires_at',
-    ),
-    'values' => 
-    array (
-      'label' => 'values',
-    ),
-    'assetType' => 
-    array (
-      'name' => 
-      array (
-        'label' => 'assetType.name',
-      ),
-    ),
-    'description' => 
-    array (
-      'label' => 'description',
-    ),
-    'parent_id' => 
-    array (
-      'label' => 'parent_id',
-    ),
-  ),
-);
+);
