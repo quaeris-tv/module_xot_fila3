71ff2caf96be90b327e2fb6e20f792275b69ef20
marco76tv <marco.sottana@gmail.com>
2024-12-04 12:04:08 +0100

    up

--- Actions/Filament/Block/GetViewBlocksOptionsByTypeAction.php
+++ Actions/Filament/Block/GetViewBlocksOptionsByTypeAction.php
@@ -11,6 +11,7 @@ namespace Modules\Xot\Actions\Filament\Block;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use Modules\Xot\Actions\File\FixPathAction;
 use Spatie\QueueableAction\QueueableAction;
 
 class GetViewBlocksOptionsByTypeAction
@@ -27,17 +28,23 @@ class GetViewBlocksOptionsByTypeAction
 
         $opts = Arr::mapWithKeys(
             $files,
-            function ($path) {
-                $module_low = Str::of($path)->between(DIRECTORY_SEPARATOR.'Modules'.DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR)
+            function ($path) use ($img) {
+                $path = app(FixPathAction::class)->execute($path);
+                $module_low = Str::of($path)
+                    ->between(DIRECTORY_SEPARATOR.'Modules'.DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR)
                     ->lower()
                     ->toString();
                 $info = pathinfo($path);
                 $name = Str::of($info['basename'])->before('.blade.php')->toString();
                 $view = $module_low.'::components.blocks.hero.'.$name;
-                $img = app(\Modules\Xot\Actions\File\AssetAction::class)
-                    ->execute($module_low.'::img/screenshots/'.$name.'.png');
+                if ($img) {
+                    $img_path = app(\Modules\Xot\Actions\File\AssetAction::class)
+                        ->execute($module_low.'::img/screenshots/'.$name.'.png');
 
-                return [$view => $img];
+                    return [$view => $img_path];
+                }
+
+                return [$view => $name];
             }
         );
 
