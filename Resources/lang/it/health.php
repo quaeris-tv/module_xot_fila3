7969fc1a8c75be53570355eec986590a27e3cec2
marco76tv <marco76tv@users.noreply.github.com>
2024-12-09 15:54:23 +0000

    Check & fix styling

--- Resources/lang/it/health.php
+++ Resources/lang/it/health.php
@@ -1,38 +1,33 @@
-<?php return array (
-  'navigation' => 
-  array (
-    'name' => 'Salute',
-    'plural' => 'Salute',
-    'group' => 
-    array (
-      'name' => 'Admin',
-    ),
-  ),
-  'pages' => 
-  array (
-    'health_check_results' => 
-    array (
-      'buttons' => 
-      array (
-        'refresh' => 'Refresh',
-      ),
-      'heading' => 'Application Health',
-      'navigation' => 
-      array (
-        'group' => 'Settings',
-        'label' => 'Application Health',
-      ),
-      'notifications' => 
-      array (
-        'check_results' => 'Check results from',
-      ),
-    ),
-  ),
-  'actions' => 
-  array (
-    'refresh' => 
-    array (
-      'label' => 'refresh',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'navigation' => [
+        'name' => 'Salute',
+        'plural' => 'Salute',
+        'group' => [
+            'name' => 'Admin',
+        ],
+    ],
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Refresh',
+            ],
+            'heading' => 'Application Health',
+            'navigation' => [
+                'group' => 'Settings',
+                'label' => 'Application Health',
+            ],
+            'notifications' => [
+                'check_results' => 'Check results from',
+            ],
+        ],
+    ],
+    'actions' => [
+        'refresh' => [
+            'label' => 'refresh',
+        ],
+    ],
+];
