249a83c0adce8b15a7465c9af3ef547c70ac01fc
marco76tv <marco.sottana@gmail.com>
2024-12-17 12:38:51 +0100

    up

--- Filament/Resources/XotBaseResource/Pages/XotBaseManageRelatedRecords.php
+++ Filament/Resources/XotBaseResource/Pages/XotBaseManageRelatedRecords.php
@@ -4,16 +4,30 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Resources\XotBaseResource\Pages;
 
-use Illuminate\Database\Eloquent\Model;
-use Modules\Xot\Filament\Traits\HasXotTable;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\ManageRelatedRecords;
+use Modules\Xot\Filament\Traits\HasXotTable;
 use Modules\Xot\Filament\Traits\NavigationLabelTrait;
 
-abstract class XotBaseManageRelatedRecords extends ManageRelatedRecords
+abstract class XotBaseManageRelatedRecords extends ManageRelatedRecords implements HasForms
 {
+    use InteractsWithForms;
     use HasXotTable;
     use NavigationLabelTrait;
 
     // protected static string $resource;
 
+    public function getFormSchema(): array
+    {
+        return [];
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            // ->model($this->getRecord()) // Assicurati di associare il record
+            ->schema($this->getFormSchema());
+    }
 }
