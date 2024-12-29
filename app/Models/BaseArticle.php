7541faf6ff5bd14296c9ca60a9962d0f0873dc98
marco76tv <marco.sottana@gmail.com>
2024-12-29 15:44:46 +0100

    up

---
+++ app/Models/BaseArticle.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Models;
+
+use Spatie\Tags\HasTags;
+
+/**
+ * Class BaseArticle.
+ *
+ * Base class for articles extending Spatie's HasTags.
+ */
+abstract class BaseArticle extends HasTags
+{
+    // Common properties and methods for articles can be added here.
+}
