0fd1e439099bf829e2bfcb069132bc17de4a2736
marco76tv <marco.sottana@gmail.com>
2025-01-01 21:34:44 +0100

    up

--- app/Filament/Resources/XotBaseResource.php
+++ app/Filament/Resources/XotBaseResource.php
@@ -4,9 +4,104 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Resources;
 
-use Filament\Resources\Resource;
+use Filament\Pages\SubNavigationPosition;
+use Filament\Resources\Resource as FilamentResource;
+use Illuminate\Support\Str;
+use Modules\Xot\Actions\ModelClass\CountAction;
+use Modules\Xot\Filament\Traits\NavigationLabelTrait;
+use Webmozart\Assert\Assert;
 
-abstract class XotBaseResource extends Resource
+abstract class XotBaseResource extends FilamentResource
 {
-    // Aggiungi qui eventuali metodi o proprietà comuni a tutte le risorse
+    use NavigationLabelTrait;
+
+    protected static ?string $model = null;
+
+    // protected static ?string $navigationIcon = 'heroicon-o-bell';
+    // protected static ?string $navigationLabel = 'Custom Navigation Label';
+    // protected static ?string $activeNavigationIcon = 'heroicon-s-document-text';
+    // protected static bool $shouldRegisterNavigation = false;
+    // protected static ?string $navigationGroup = 'Parametri di Sistema';
+    protected static ?int $navigationSort = 3;
+    protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::Top;
+
+    public static function getModuleName(): string
+    {
+        return Str::between(static::class, 'Modules\\', '\Filament');
+    }
+
+    public function hasCombinedRelationManagerTabsWithContent(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    public static function getModel(): string
+    {
+        // if (null != static::$model) {
+        //    return static::$model;
+        // }
+        $moduleName = static::getModuleName();
+        $modelName = Str::before(class_basename(static::class), 'Resource');
+        $res = 'Modules\\'.$moduleName.'\Models\\'.$modelName;
+        Assert::classExists($res, sprintf('Model class %s does not exist', $res));
+        Assert::subclassOf($res, \Illuminate\Database\Eloquent\Model::class, sprintf('Class %s must extend Eloquent Model', $res));
+        static::$model = $res;
+
+        return $res;
+    }
+
+    public static function extendTableCallback(): array
+    {
+        return [
+        ];
+    }
+
+    public static function extendFormCallback(): array
+    {
+        return [
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        try {
+            /*
+            //return (string) static::getModel()::count();
+            $model = app(static::getModel());
+            $table = $model->getTable();
+            $db = $model->getConnection()->getDatabaseName();
+            if ($db == ':memory:') {
+                return number_format($model->count(), 0);
+            }
+            $count = DB::table('information_schema.TABLES')
+            ->where('TABLE_SCHEMA', $databaseName)
+            ->where('TABLE_NAME', $tableName)
+            ->value('TABLE_ROWS');
+            return $db;
+            */
+            $count = app(CountAction::class)->execute(static::getModel());
+
+            return number_format($count, 0);
+        } catch (\Exception $e) {
+            return '---';
+        }
+    }
+
+    public static function getPages(): array
+    {
+        $prefix = static::class.'\Pages\\';
+        $name = Str::of(class_basename(static::class))->before('Resource')->toString();
+        $index = Str::of($prefix)->append('List'.$name.'s')->toString();
+        $create = Str::of($prefix)->append('Create'.$name.'')->toString();
+        $edit = Str::of($prefix)->append('Edit'.$name.'')->toString();
+
+        return [
+            'index' => $index::route('/'),
+            'create' => $create::route('/create'),
+            'edit' => $edit::route('/{record}/edit'),
+        ];
+    }
 }
