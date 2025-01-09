61added0806e5e6a383c102870c1ce2185934057
marco <marco.sottana@gmail.com>
2025-01-09 16:35:16 +0100

    up

--- app/Filament/Resources/CacheLockResource/Pages/ListCacheLocks.php
+++ app/Filament/Resources/CacheLockResource/Pages/ListCacheLocks.php
@@ -7,10 +7,26 @@ namespace Modules\Xot\Filament\Resources\CacheLockResource\Pages;
 use Filament\Actions;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 use Modules\Xot\Filament\Resources\CacheLockResource;
+use Filament\Tables\Columns\TextColumn;
 
 class ListCacheLocks extends XotBaseListRecords
 {
     protected static string $resource = CacheLockResource::class;
 
-    
+    public function getListTableColumns(): array
+    {
+        return [
+            'key' => TextColumn::make('key')
+                ->searchable()
+                ->sortable()
+                ->wrap(),
+            'owner' => TextColumn::make('owner')
+                ->searchable()
+                ->sortable()
+                ->wrap(),
+            'expiration' => TextColumn::make('expiration')
+                ->numeric()
+                ->sortable(),
+        ];
+    }
 }
