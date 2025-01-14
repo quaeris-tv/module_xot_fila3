511198d48441c36c99afd894ccf1fb1b4f27bab7
marco <marco.sottana@gmail.com>
2025-01-14 18:53:25 +0100

    up

---
+++ lang/it/set_default_tenant_for_urls.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'actions' => 
+  array (
+    'authenticate' => 
+    array (
+      'label' => 'authenticate',
+    ),
+  ),
+  'fields' => 
+  array (
+    'email' => 
+    array (
+      'label' => 'email',
+    ),
+    'password' => 
+    array (
+      'label' => 'password',
+    ),
+    'remember' => 
+    array (
+      'label' => 'remember',
+    ),
+  ),
+);
