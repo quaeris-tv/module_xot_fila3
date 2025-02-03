ae3dc9173d52cfac525bc8b65a01f8073d546828
marco76tv <marco.sottana@gmail.com>
2025-02-03 21:18:32 +0100

    Auto update submodule

--- app/Filament/Resources/Pages/XotBaseCreateRecord.php
+++ app/Filament/Resources/Pages/XotBaseCreateRecord.php
@@ -4,38 +4,15 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Resources\Pages;
 
-use Filament\Forms;
 use Filament\Resources\Pages\CreateRecord;
 
 abstract class XotBaseCreateRecord extends CreateRecord
 {
-    /**
-     * Get the form schema with common fields.
-     */
-    public function form(Forms\Form $form): Forms\Form
-    {
-        return $form
-            ->schema($this->getFormSchema())
-            // ->columns($this->getFormColumns())
-            // ->statePath('data')
-        ;
-    }
-
     /**
      * Get the number of form columns.
      */
     protected function getFormColumns(): int|array
     {
         return 1;
-    }
-
-    /**
-     * Get the form schema.
-     *
-     * @return array<Forms\Components\Component>
-     */
-    protected function getFormSchema(): array
-    {
-        return [];
     }
 }
--- app/Filament/Resources/Pages/XotBaseEditRecord.php
+++ app/Filament/Resources/Pages/XotBaseEditRecord.php
@@ -4,41 +4,16 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Resources\Pages;
 
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
 
 abstract class XotBaseEditRecord extends EditRecord
 {
     /**
-     * Get the form schema with common fields.
-     */
-    public function formOLD(Form $form): Form
-    {
-        dddx($this->getFormSchema());
-
-        return $form
-            ->schema($this->getFormSchema())
-            // ->columns($this->getFormColumns())
-            // ->statePath('data')
-        ;
-    }
-
-    /**
      * Get the number of form columns.
      */
     protected function getFormColumns(): int|array
     {
         return 1;
-    }
-
-    /**
-     * Get the form schema.
-     *
-     * @return array<Forms\Components\Component>
-     */
-    protected function getFormSchema(): array
-    {
-        return [];
     }
 }
