e820f7f3d5696e7fd3abff9bc60c0993178a2acb
marco76tv <marco.sottana@gmail.com>
2025-02-03 20:37:14 +0100

    up

--- app/Filament/Resources/CacheResource/Pages/ListCaches.php
+++ app/Filament/Resources/CacheResource/Pages/ListCaches.php
@@ -14,6 +14,9 @@ use Modules\Xot\Filament\Resources\CacheResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 use Modules\Xot\Filament\Widgets\Clock;
 
+/**
+ * @see CacheResource
+ */
 class ListCaches extends XotBaseListRecords
 {
     public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
@@ -27,26 +30,31 @@ class ListCaches extends XotBaseListRecords
         ];
     }
 
-    /**
-     * @return array<string, Tables\Columns\Column>
-     */
     public function getListTableColumns(): array
     {
         return [
-            'key' => TextColumn::make('key'),
-            'value' => TextColumn::make('value'),
-            'expiration' => TextColumn::make('expiration'),
+            'key' => TextColumn::make('key')
+                ->searchable()
+                ->sortable()
+                ->wrap()
+                ->label('Key'),
+
+            'value' => TextColumn::make('value')
+                ->searchable()
+                ->wrap()
+                ->label('Value'),
+
+            'expiration' => TextColumn::make('expiration')
+                ->dateTime()
+                ->sortable()
+                ->label('Expiration'),
         ];
     }
 
     public function getGridTableColumns(): array
     {
         return [
-            Stack::make([
-                TextColumn::make('key'),
-                TextColumn::make('value'),
-                TextColumn::make('exipiration'),
-            ]),
+            Stack::make($this->getListTableColumns()),
         ];
     }
 
