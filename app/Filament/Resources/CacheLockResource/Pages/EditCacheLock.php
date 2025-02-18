9a8f975182439d581fb5a8c09448dcbacd98cb04
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-02-18 20:46:32 +0000

    Check & fix styling

--- app/Filament/Resources/CacheLockResource/Pages/EditCacheLock.php
+++ app/Filament/Resources/CacheLockResource/Pages/EditCacheLock.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Resources\CacheLockResource\Pages;
 
-use Filament\Actions;
 use Modules\Xot\Filament\Resources\CacheLockResource;
 
 class EditCacheLock extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
 {
     protected static string $resource = CacheLockResource::class;
-
-   
 }
