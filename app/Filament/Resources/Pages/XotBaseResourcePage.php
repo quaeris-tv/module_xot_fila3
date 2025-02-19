bbb0d20f77eb0503db821f8b1f8ac258c8b983d7
marco76tv <marco.sottana@gmail.com>
2025-02-19 19:06:38 +0100

    .

--- app/Filament/Resources/Pages/XotBaseResourcePage.php
+++ app/Filament/Resources/Pages/XotBaseResourcePage.php
@@ -6,11 +6,17 @@ namespace Modules\Xot\Filament\Resources\Pages;
 
 use Filament\Resources\Pages\Page as FilamentPage;
 
+<<<<<<< HEAD
 /**
  * Base Resource Page for Xot Module
  * Extends Filament's Page class to provide common functionality
  */
 abstract class XotBaseResourcePage extends FilamentPage
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+abstract class XotBaseResourcePage extends FilamentResourcePage
+>>>>>>> f7282529 (.)
 {
     /**
      * Get view data for the page
@@ -32,3 +38,12 @@ abstract class XotBaseResourcePage extends FilamentPage
         return [];
     }
 }
+<<<<<<< HEAD
+=======
+=======
+abstract class XotBaseResourcePage extends Page {}
+>>>>>>> 2f0934a2 (up)
+=======
+abstract class XotBaseResourcePage extends Page {}
+>>>>>>> 94ea520a (up)
+>>>>>>> f7282529 (.)
