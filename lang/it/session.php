d3511c403c1c1f641d8252917338a42a16372a63
marco76tv <marco.sottana@gmail.com>
2025-02-19 11:41:07 +0100

    .

--- lang/it/session.php
+++ lang/it/session.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<?php return array (
-  'navigation' => 
-  array (
-    'name' => 'Sessioni',
-    'plural' => 'Sessioni',
-    'group' => 
-    array (
-      'name' => 'Sistema',
-      'description' => 'Gestione delle sessioni utente',
-    ),
-    'sort' => 88,
-    'label' => 'Gestione Sessioni',
-    'icon' => 'heroicon-o-user-circle',
-  ),
-  'pages' => 
-  array (
-    'health_check_results' => 
-    array (
-      'buttons' => 
-      array (
-        'refresh' => 'Aggiorna',
-      ),
-      'heading' => 'Stato Sessioni',
-      'navigation' => 
-      array (
-        'group' => 'Sistema',
-        'label' => 'Stato Sessioni',
-      ),
-      'notifications' => 
-      array (
-        'check_results' => 'Risultati del controllo da',
-      ),
-    ),
-  ),
-);
-=======
 <?php
 
 declare(strict_types=1);
@@ -207,4 +170,3 @@ return [
         ],
     ],
 ];
->>>>>>> origin/dev
