9fb3d5cf5deb890e42efd91b51a935a91ed26064
marco76tv <marco.sottana@gmail.com>
2025-02-27 16:04:01 +0100

    fix(XotBaseWidget): add missing use statements and correct constructor implementation to properly set the view feat(XotBaseWidget): add support for page filters, title, icon, and view configuration using Filament's widget features

--- app/Filament/Widgets/XotBaseWidget.php
+++ app/Filament/Widgets/XotBaseWidget.php
@@ -4,24 +4,36 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Widgets;
 
+<<<<<<< HEAD
 use Filament\Widgets\Widget as FilamentWidget;
+=======
+>>>>>>> d5e9f6d7 (.)
 use Illuminate\Support\Facades\Cache;
+use Filament\Widgets\WidgetConfiguration;
+use Filament\Widgets\Widget as FilamentWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
+use Modules\Xot\Actions\View\GetViewByClassAction;
 
 /**
  * @property bool $shouldRender
  */
 abstract class XotBaseWidget extends FilamentWidget
 {
-    /**
-     * The cache key for the widget.
-     */
-    protected ?string $cacheKey = null;
+    use InteractsWithPageFilters;
+    public string $title = '';
+    public string $icon = '';
+    protected static string $view = 'ui::empty';
 
 
     public function __construct()
     {
         //parent::__construct();//Cannot call constructor
+<<<<<<< HEAD
         $view = app(GetViewByClassAction::class)->execute(static::class);
         static::$view = $view;
+=======
+        $view=app(GetViewByClassAction::class)->execute(static::class);
+        static::$view=$view;
+>>>>>>> d5e9f6d7 (.)
     }
 }
