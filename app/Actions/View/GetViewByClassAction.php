9a4957836fb368c377d935a59157f0cc752a1155
marco76tv <marco.sottana@gmail.com>
2025-02-28 10:05:31 +0100

    Here is a possible commit message:
    "Update health SVG icon to include heartbeat animation and pulse effect"
    
    This commit message is concise, informative, and descriptive. It clearly states the purpose of the update (to add an animation and pulse effect) and provides context about what was changed in the SVG file.

--- app/Actions/View/GetViewByClassAction.php
+++ app/Actions/View/GetViewByClassAction.php
@@ -16,29 +16,74 @@ class GetViewByClassAction
     /**
      * "Modules\UI\Filament\Widgets\GroupWidget" => "ui::filament.widgets.group"
      */
+<<<<<<< HEAD
     public function execute(string $class, string $suffix = ''): string
     {
         $module = Str::of($class)->betweenFirst('Modules\\', '\\')->toString();
         $module_low = Str::of($module)->lower()->toString();
         $after = Str::of($class)
+=======
+<<<<<<< HEAD
+    public function execute(string $class, string $suffix=''): string
+    {
+        $module = Str::of($class)->betweenFirst('Modules\\', '\\')->toString();
+        $module_low = Str::of($module)->lower()->toString();
+        $after=Str::of($class)
+=======
+    public function execute(string $class, string $suffix = ''): string
+    {
+        $module = Str::of($class)->betweenFirst('Modules\\', '\\')->toString();
+        $module_low = Str::of($module)->lower()->toString();
+        $after = Str::of($class)
+>>>>>>> 755e82bc (.)
+>>>>>>> 08c95fb8 (.)
             ->after('Modules\\'.$module.'\\')
             ->explode('\\')
             ->toArray();
 
         $mapped = Arr::map($after, function (string $value, int $key) use ($after) {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+            if($key>0 && isset($after[$key-1])) {
+                $singular = Str::of($after[$key-1])->singular()->toString();
+                if(Str::endsWith($value, $singular)) {
+                    $value=Str::of($value)->beforeLast($singular)->toString();
+=======
+>>>>>>> 08c95fb8 (.)
             if ($key > 0 && isset($after[$key - 1])) {
                 $singular = Str::of($after[$key - 1])->singular()->toString();
                 if (Str::endsWith($value, $singular)) {
                     $value = Str::of($value)->beforeLast($singular)->toString();
+<<<<<<< HEAD
+=======
+>>>>>>> 755e82bc (.)
+>>>>>>> 08c95fb8 (.)
                 }
             }
             return Str::of($value)->slug()->toString();
         });
 
+<<<<<<< HEAD
         $implode = implode('.', $mapped);
         $view = $module_low.'::'.$implode.$suffix;
 
         return $view;
 
+=======
+<<<<<<< HEAD
+        $implode=implode('.', $mapped);
+        $view=$module_low.'::'.$implode.$suffix;
+
+        return $view;
+        
+=======
+        $implode = implode('.', $mapped);
+        $view = $module_low.'::'.$implode.$suffix;
+
+        return $view;
+
+>>>>>>> 755e82bc (.)
+>>>>>>> 08c95fb8 (.)
     }
 }
