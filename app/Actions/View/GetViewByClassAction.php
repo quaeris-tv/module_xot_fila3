a751c182f866fd93a9c5486728e329527a3c3034
marco76tv <marco.sottana@gmail.com>
2025-02-28 09:47:56 +0100

    fix(GetViewByClassAction): fix constructor call and add missing import feat(XotBaseWidget): use GetViewByClassAction to determine view class chore(composer.json): update aaronfrancis/fast-paginate version fix(composer.json): update barryvdh/laravel-debugbar version

--- app/Actions/View/GetViewByClassAction.php
+++ app/Actions/View/GetViewByClassAction.php
@@ -16,29 +16,52 @@ class GetViewByClassAction
     /**
      * "Modules\UI\Filament\Widgets\GroupWidget" => "ui::filament.widgets.group"
      */
+<<<<<<< HEAD
     public function execute(string $class, string $suffix = ''): string
     {
         $module = Str::of($class)->betweenFirst('Modules\\', '\\')->toString();
         $module_low = Str::of($module)->lower()->toString();
         $after = Str::of($class)
+=======
+    public function execute(string $class, string $suffix=''): string
+    {
+        $module = Str::of($class)->betweenFirst('Modules\\', '\\')->toString();
+        $module_low = Str::of($module)->lower()->toString();
+        $after=Str::of($class)
+>>>>>>> 8045aaff (up)
             ->after('Modules\\'.$module.'\\')
             ->explode('\\')
             ->toArray();
 
         $mapped = Arr::map($after, function (string $value, int $key) use ($after) {
+<<<<<<< HEAD
             if ($key > 0 && isset($after[$key - 1])) {
                 $singular = Str::of($after[$key - 1])->singular()->toString();
                 if (Str::endsWith($value, $singular)) {
                     $value = Str::of($value)->beforeLast($singular)->toString();
+=======
+            if($key>0 && isset($after[$key-1])) {
+                $singular = Str::of($after[$key-1])->singular()->toString();
+                if(Str::endsWith($value, $singular)) {
+                    $value=Str::of($value)->beforeLast($singular)->toString();
+>>>>>>> 8045aaff (up)
                 }
             }
             return Str::of($value)->slug()->toString();
         });
 
+<<<<<<< HEAD
         $implode = implode('.', $mapped);
         $view = $module_low.'::'.$implode.$suffix;
 
         return $view;
 
+=======
+        $implode=implode('.', $mapped);
+        $view=$module_low.'::'.$implode.$suffix;
+
+        return $view;
+        
+>>>>>>> 8045aaff (up)
     }
 }
--- app/Filament/Widgets/XotBaseWidget.php
+++ app/Filament/Widgets/XotBaseWidget.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Widgets;
 
+<<<<<<< HEAD
 use Filament\Widgets\Widget as FilamentWidget;
 use Illuminate\Support\Facades\Cache;
 use Filament\Widgets\WidgetConfiguration;
+=======
+use Illuminate\Support\Facades\Cache;
+use Filament\Widgets\WidgetConfiguration;
+use Filament\Widgets\Widget as FilamentWidget;
+>>>>>>> 8045aaff (up)
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Modules\Xot\Actions\View\GetViewByClassAction;
 
@@ -20,11 +26,20 @@ abstract class XotBaseWidget extends FilamentWidget
     public string $icon = '';
     protected static string $view = 'ui::empty';
 
+<<<<<<< HEAD
 
     public function __construct()
     {
         //parent::__construct();//Cannot call constructor
         $view = app(GetViewByClassAction::class)->execute(static::class);
         static::$view = $view;
+=======
+    
+    public function __construct()
+    {
+        //parent::__construct();//Cannot call constructor
+        $view=app(GetViewByClassAction::class)->execute(static::class);
+        static::$view=$view;
+>>>>>>> 8045aaff (up)
     }
 }
