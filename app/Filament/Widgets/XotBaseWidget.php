6f485aed0a8ab1461e2eec3d075a7aeb5c7c907b
marco76tv <marco.sottana@gmail.com>
2025-02-28 09:52:39 +0100

    fix(XotBaseWidget): add missing use statements and correct constructor implementation to properly set the view fix(XotBaseWidget): remove duplicate use statement for FilamentWidget

--- app/Filament/Widgets/XotBaseWidget.php
+++ app/Filament/Widgets/XotBaseWidget.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Widgets;
 
-<<<<<<< HEAD
 use Filament\Widgets\Widget as FilamentWidget;
-=======
->>>>>>> d5e9f6d7 (.)
 use Illuminate\Support\Facades\Cache;
 use Filament\Widgets\WidgetConfiguration;
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 use Filament\Widgets\Widget as FilamentWidget;
 >>>>>>> 34f9e999 (fix(XotBaseWidget): add missing use statements and correct constructor implementation to properly set the view)
+=======
+>>>>>>> 818820bc (fix(XotBaseWidget): remove duplicate use statement for FilamentWidget)
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Modules\Xot\Actions\View\GetViewByClassAction;
 
@@ -39,6 +39,7 @@ abstract class XotBaseWidget extends FilamentWidget
         static::$view = $view;
 =======
         //parent::__construct();//Cannot call constructor
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         $view=app(GetViewByClassAction::class)->execute(static::class);
@@ -188,6 +189,13 @@ abstract class XotBaseWidget extends FilamentWidget
         static::$view=$view;
 >>>>>>> d5e9f6d7 (.)
 >>>>>>> 9fb3d5cf (fix(XotBaseWidget): add missing use statements and correct constructor implementation to properly set the view)
+<<<<<<< HEAD
 >>>>>>> 34f9e999 (fix(XotBaseWidget): add missing use statements and correct constructor implementation to properly set the view)
+=======
+=======
+        $view = app(GetViewByClassAction::class)->execute(static::class);
+        static::$view = $view;
+>>>>>>> fda02dac (fix(XotBaseWidget): remove duplicate use statement for FilamentWidget)
+>>>>>>> 818820bc (fix(XotBaseWidget): remove duplicate use statement for FilamentWidget)
     }
 }
